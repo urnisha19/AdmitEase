1fcd64d20547942bb08293de76d447ca6da2faeb
urnisha19 <urnisha09@gmail.com>
2025-07-27 02:57:26 +0600

    updated

--- src/Pages/AdmissionPage/AdmissionForm.jsx
+++ src/Pages/AdmissionPage/AdmissionForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const AdmissionForm = ({ selectedCollege, onSuccess }) => {
   const [formData, setFormData] = useState({
